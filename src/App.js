35741f22630653049d4655ffd1512a24a26bcdec
balajisaikumar200 <balajisaikumar3@gmail.com>
2022-05-22 16:23:09 +0530

    added grid selctor buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import './App.css';
-import Grid from './components/Grid'
-import MainHeader from './components/Header/MainHeader'
-import {useState} from 'react'
+import "./App.css";
+import Grid from "./components/Grid";
+import MainHeader from "./components/Header/MainHeader";
+import GridSelector from "./components/GridSelector";
+import { useState } from "react";
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState(' ');
-  function onChange(event){
+  const [currentGrid, setCurrentGrid] = useState();
+  const [gridActive, setGridActive] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(" ");
+  function onChange(event) {
     const val = event.target.value;
-    setSelectedOption((prev) => val)
+    setSelectedOption((prev) => val);
   }
+  function gridChange(event) {
+    setGridActive(true);
+    const val = event.target.value;
+    setCurrentGrid((prev) => Number(val));
+  }
+
   return (
     <>
-      <MainHeader onChange={onChange} />
-      <Grid selectedOption={selectedOption} /> 
+      <GridSelector gridChange={gridChange} />
+      {gridActive ? (
+        <div>
+          <MainHeader onChange={onChange} />
+          <Grid selectedOption={selectedOption} currentGrid={currentGrid} />
+        </div>
+      ) : (
+        console.log("nothing here")
+      )}
     </>
   );
 }
