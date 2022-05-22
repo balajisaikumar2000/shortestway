8a3f3ea003dbc5e5cbae8d068468ec32bbb32434
Sree krishna <sreekrishnakaturi@gmail.com>
2022-05-22 08:41:10 +0530

    fixed bug in grid update and added feat to select only one cell as Start or End

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,10 +4,34 @@ import './Grid.css'
 function Grid(props) {
     // const [grid, setGrid] = useState(() => new Array(10).fill('').map(() => new Array(15).fill(' '))); 
     const grid = new Array(15).fill('').map(() => new Array(30).fill(' '));
+    const [start ,setStart] = useState(false);
+    const [end,setEnd] = useState(false); 
     function handleClick(event) {
         const cellId = event.target.id;
         const [rowId, colId] = cellId.split('-').map(Number);
-        grid[rowId][colId] = props.selectedOption;
+        if (props.selectedOption === 'S'){
+            setStart(prev => !prev)
+            if (start){
+                grid[rowId][colId] = 'S';
+            }
+            else{
+                grid[rowId][colId] = ' ';
+            }
+        }else if(props.selectedOption === 'E'){
+            setEnd(prev => !prev)
+            if (end){
+                grid[rowId][colId] = 'E';
+            }
+            else{
+                grid[rowId][colId] = ' ';
+            }
+        }else{
+            if(props.selectedOption === grid[rowId][colId]){
+                grid[rowId][colId] = ' ';
+            }else{
+                grid[rowId][colId] = props.selectedOption;
+            }
+        }
         // setGrid((prevGrid) => {
         //     prevGrid[rowId][colId] = props.selectedOption;
         //     return [...prevGrid]
@@ -20,7 +44,7 @@ function Grid(props) {
                     return <div className="row" id={"row_" + rowId} key={rowId}>
                         {
                             row.map((cell, cellId) => {
-                                return <Cell selectedOption={props.selectedOption} rowId={rowId} cellId={cellId} key={cellId} onClick={handleClick}></Cell>
+                                return <Cell selectedOption={props.selectedOption} start={start} end={end} rowId={rowId} cellId={cellId} key={cellId} onClick={handleClick}></Cell>
                             }
                             )
                         }
@@ -34,13 +58,18 @@ function Grid(props) {
 function Cell(props) {
     const [cell, cellState] = useState(() => props.selectedOption) // # passing arg as function makes it run only on first time
     function handleClick(event) {
+        let invalidState = false;
         cellState((prev) => {
             if (props.selectedOption === prev) {
                 return ' ';
+            }else if( (props.selectedOption === 'S' && props.start) || (props.selectedOption === 'E' && props.end)){
+                invalidState = true;
+                alert('You can choose only cell as Start or End')
+                return ' ';
             }
             return props.selectedOption;
-        })
-        props.onClick(event)
+        });
+        if( !invalidState ) props.onClick(event);
     }
     return (
         <div className={`cell ${cell}`} onClick={handleClick} id={`${props.rowId}-${props.cellId}`} ><p>{`${props.rowId}-${props.cellId}`}</p></div>
