c4ce55a43d3aee4a0d71e1c66afc9737f1a24f5f
balajisaikumar200 <balajisaikumar3@gmail.com>
2022-05-23 11:12:37 +0530

    pulled some changes

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,39 +1,29 @@
-import React from 'react'
-import Options from './Options'
+import React from "react";
+import Options from "./Options";
 
-<<<<<<< HEAD
-function MainHeader() {
-    const cellFillOptions = [ 
-        {id:'Start', value: 's'}, 
-        {id:'End' , value:'e'}, 
-        {id:'Wall' , value:'w'},
-        {id:'Clear' , value:'c'}
-    ];
-
-  return (
-    <header>
-        <Options name='cellFillOptions' options={cellFillOptions} />
-=======
 function MainHeader(props) {
-    const cellFillOptions = [ 
-        {id:'Start', value: 'S'}, 
-        {id:'End' , value:'E'}, 
-        {id:'Wall' , value:'W'},
-        {id:'Clear' , value:' '}
-    ];
+  const cellFillOptions = [
+    { id: "Start", value: "S" },
+    { id: "End", value: "E" },
+    { id: "Wall", value: "W" },
+    { id: "Clear", value: " " },
+  ];
 
-    function onChange(event) {
-      props.onChange(event)
-    }
+  function onChange(event) {
+    props.onChange(event);
+  }
 
   return (
     <header>
-        <Options name='cellFillOptions' options={cellFillOptions} onChange={onChange}/>
-        <button>Reset</button>
-        <button>Find Path</button>
->>>>>>> krishna/test
+      <Options
+        name="cellFillOptions"
+        options={cellFillOptions}
+        onChange={onChange}
+      />
+      <button>Reset</button>
+      <button>Find Path</button>
     </header>
-  )
+  );
 }
 
 export default MainHeader;
--- src/components/Header/Options.js
+++ src/components/Header/Options.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import classes from './Options.module.css'
-
+import React from "react";
+import classes from "./Options.module.css";
 
 function Options(props) {
-    function createOption(item,index){
-        return (
-        <span key={index} className={classes["radio-option"]}>
-<<<<<<< HEAD
-            <input type="radio" id={item.id} name={props.name} value={item.value} />
-            <label htmlFor={item.id}>{item.id}</label>
-        </span>)
-    }
-=======
-            <input type="radio" id={item.id} name={props.name} value={item.value} onChange={props.onChange}/>
-            <label htmlFor={item.id}>{item.id}</label>
-        </span>)
-    }
-
->>>>>>> krishna/test
+  function createOption(item, index) {
     return (
-    <div className= { classes["radio-toolbar"] } > 
-        { props.options.map(createOption) }
+      <span key={index} className={classes["radio-option"]}>
+        <input
+          type="radio"
+          id={item.id}
+          name={props.name}
+          value={item.value}
+          onChange={props.onChange}
+        />
+        <label htmlFor={item.id}>{item.id}</label>
+      </span>
+    );
+  }
+
+  return (
+    <div className={classes["radio-toolbar"]}>
+      {props.options.map(createOption)}
     </div>
-  )
+  );
 }
 
-export default Options
+export default Options;
